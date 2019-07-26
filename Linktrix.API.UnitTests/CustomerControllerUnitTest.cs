2df4fa1211498d7b2e7c3ff75e0d6262701a1145
a.nugraha <a.nugraha@allegrodev.com>
2019-07-26 14:13:12 +0700

    add customer unit test

--- Linktrix.API.UnitTests/CustomerControllerUnitTest.cs
+++ Linktrix.API.UnitTests/CustomerControllerUnitTest.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
 using Linktrix.API.Controllers;
 using Linktrix.API.Mapping;
+using Linktrix.API.Persistence.Contexts;
 using Linktrix.API.Persistence.Repositories;
+using Linktrix.API.Resources;
 using Linktrix.API.Services;
 using Linktrix.API.UnitTests.Context;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,24 +16,71 @@ namespace Linktrix.API.UnitTests
 {
     public class CustomerControllerUnitTest
     {
-        [Fact]
-        public async Task TestGetAllCustomerAsync()
+        public CustomersController ArrangeController(AppDbContext dbContext)
         {
-            //var dbContext = DbContextMocker.GetAppDbContext(nameof(TestGetAllCustomerAsync));
-            //var customerRepository = new CustomerRepository(dbContext);
-            //var customerService = new CustomerService(customerRepository);
-            //var config = new MapperConfiguration(cfg =>
-            //{
-            //    cfg.AddProfile(new ModelToResourceProfile());
-            //});
-            //var mapper = config.CreateMapper();
-            //var controller = new CustomersController(customerService, mapper);
+            var customerRepository = new CustomerRepository(dbContext);
+            var unitOfWork = new UnitOfWork(dbContext);
+            var customerService = new CustomerService(customerRepository, unitOfWork);
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ModelToResourceProfile());
+            });
+            var mapper = config.CreateMapper();
 
-            //var response = await controller.GetAllAsync();
+            return new CustomersController(customerService, mapper);
+        }
 
-            //dbContext.Dispose();
+        [Fact]
+        public async Task TestGetAllAsync()
+        {
+            //ARRANGE
+            var dbContext = DbContextMocker.GetAppDbContext(nameof(TestGetCustomerByIdAsync));
+            var controller = ArrangeController(dbContext);
 
-            //Assert.False(value == null);
+            //ACT
+            var response = await controller.GetAllAsync() as List<CustomerResource>;
+
+            dbContext.Dispose();
+
+            //ASSERT
+            Assert.Equal(3, response.Count);
+        }
+
+        [Fact]
+        public async Task TestGetCustomerByIdAsync()
+        {
+            //ARRANGE
+            var dbContext = DbContextMocker.GetAppDbContext(nameof(TestGetCustomerByIdAsync));
+            var controller = ArrangeController(dbContext);
+
+            //ACT
+            var response1 = await controller.GetCustomerByIdAsync(123456) as ObjectResult;
+            var response2 = await controller.GetCustomerByIdAsync(1) as ObjectResult;
+
+            dbContext.Dispose();
+
+            //ASSERT
+            var value = response1.Value as CustomerResource;
+            Assert.Equal(200, response1.StatusCode);
+            Assert.Equal(123456, value.CustomerId);
+            Assert.Equal(400, response2.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestDeleteAsync()
+        {
+            //ARRANGE
+            var dbContext = DbContextMocker.GetAppDbContext(nameof(TestDeleteAsync));
+            var controller = ArrangeController(dbContext);
+
+            //ACT
+            var response = await controller.DeleteAsync(123457);
+            var allCustomer = await controller.GetAllAsync() as List<CustomerResource>;
+
+            dbContext.Dispose();
+
+            //ASSERT
+            Assert.Equal(2, allCustomer.Count);
         }
     }
 }
